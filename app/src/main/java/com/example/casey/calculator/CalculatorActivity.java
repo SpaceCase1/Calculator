b1c77ce3e55f3d1bcf178ee81a5a41c3467d0658
Casey <caseycode2@gmail.com>
2017-03-26 19:08:45 -0700

    Adding first version of main activity layout

---
+++ app/src/main/java/com/example/casey/calculator/CalculatorActivity.java
@@ -0,0 +1,13 @@
+package com.example.casey.calculator;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CalculatorActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculator);
+    }
+}
